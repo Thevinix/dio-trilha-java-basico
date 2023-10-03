f0f07d25f7f18ceed1f704133133125f93f72419
Thevinix <vinicius.a.assis99@gmail.com>
2023-10-02 22:17:48 -0300

    ToDo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // ToDo: Conhecer e importar a classe scanner
+        //  Exibir as mensagens para o nosso usuário
+        // Obter pela scanner os valores digitados no terminal
+        // Exibir a mensagem conta criada
     }
 }
